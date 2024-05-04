3553cf1f1a21fa496e6e6d5b965ca58f46d27e8c
Садыганов Даниил Олегович <iv323s22@csc.sibsutis.ru>
2024-05-04 18:56:42 +0700

    Implemented the functionality of an empty graphic window

--- src/main/main.c
+++ src/main/main.c
@@ -1,3 +1,22 @@
+#include <ncurses.h>
+
 int main(int args, char* argv[]) {
+    // Инициализация библиотеки ncurses
+    initscr();
+
+    // Создание нового окна размера 20x40
+    // с координатами левого верхнего угла (5, 10)
+    WINDOW* win = newwin(20, 40, 5, 10);
+    box(win, 0, 0);
+    wrefresh(win);
+
+    // Ожидание нажатия клавиши q для выхода
+    int ch;
+    while ((ch = getch()) != 'q')
+
+    // Очистка и завершение работы библиотеки ncurses
+    delwin(win);
+    endwin();
+
     return 0;
 }
