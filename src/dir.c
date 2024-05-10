c982c5f02854ad24e334ed20aeebea7562d1a6f2
Denis-Nosov <dnosov537@gmail.com>
2024-05-10 17:43:24 +0700

    Change dir: change path access, #4

--- src/dir.c
+++ src/dir.c
@@ -1,9 +1,8 @@
-#include <dirent.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "dir.h"
-#include "list.h"
 
 /* находим путь запускаемого приложения */
 int absolute_root_path(char** path)
@@ -28,12 +27,15 @@ void fill_listnode(DIR* dir, Listdir* ldir)
 char* change_path(char* path, char* name)
 {
     char* mod_path = malloc(strlen(path) + strlen(name) + 2);
-    mod_path[strlen(path)] = '\\';
-    strcat(mod_path, name);
+    if (mod_path == NULL)
+        return NULL;
+    memcpy(mod_path, path, strlen(path) + 1);
+    mod_path[strlen(path)] = '/';
+    memcpy(&mod_path[strlen(path) + 1], name, strlen(name) + 1);
     return mod_path;
 };
 
-/* заполнение списка каталогов */
+/* заполнение списка каталогов. ldir должен быть инициализирован */
 int fill_listdir(Listdir* ldir)
 {
     DIR* dir;
@@ -41,33 +43,35 @@ int fill_listdir(Listdir* ldir)
 
     if (absolute_root_path(&path))
         return 1;
-    listdir_init(ldir, path);
+    ldir->path_dir = path;
 
     /* заполняем в структуру содержимое корневого пути */
     dir = opendir(path);
+    if (!dir)
+        return 1;
     fill_listnode(dir, ldir);
     closedir(dir);
 
     /* увеличение связных списков и добавление каталогов с их содержимых */
-    for (Listdir* mod_ldir = NULL; ldir != NULL; ldir = ldir->next) {
-        Listnode* n = ldir->node;
+    Listdir* t_ldir = ldir;
+    for (Listdir* mod_ldir = NULL; t_ldir != NULL; t_ldir = t_ldir->next) {
+        Listnode* n = t_ldir->node;
         for (char* mod_path = NULL; n != NULL; n = n->next) {
             if (n->type == DT_DIR) {
                 if (strcmp(n->name, ".") == 0 || strcmp(n->name, "..") == 0)
                     continue;
 
-                mod_path = change_path(ldir->path_dir, n->name);
-                if (mod_path == NULL) {
-                    free(path);
+                mod_path = change_path(t_ldir->path_dir, n->name);
+                if (mod_path == NULL)
                     return 1;
-                }
-                mod_ldir = listdir_add(ldir, mod_path);
-                dir = opendir(mod_path);
+
+                mod_ldir = listdir_create(mod_path);
+                listdir_add(t_ldir, mod_ldir);
+                dir = opendir(mod_ldir->path_dir);
                 fill_listnode(dir, mod_ldir);
                 closedir(dir);
             }
         }
     }
-    free(path);
     return 0;
 };
--- src/dir.h
+++ src/dir.h
@@ -1,6 +1,10 @@
 #ifndef PATH_H
 #define PATH_H
 
+#include <dirent.h>
+
+#include "list.h"
+
 int absolute_root_path(char** path);
 void fill_listnode(DIR* dir, Listdir* ldir);
 char* change_path(char* path, char* name);
