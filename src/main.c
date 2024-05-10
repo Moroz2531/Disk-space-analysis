ec1c952f92e2f303e06e96c973e00ddb7b0defc9
Denis-Nosov <dnosov537@gmail.com>
2024-05-10 17:45:33 +0700

    Change main, #4

--- src/main.c
+++ src/main.c
@@ -1,14 +1,16 @@
-#include "parser.h"
+#include "dir.h"
 #include "help.h"
+#include "parser.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[])
+{
     argv_t new_argv = argv_parse(argc, argv);
     int ch;
     // опция '-h'
     if (new_argv.opt && strcmp(new_argv.opt, "-h") == 0) {
         help_draw();
         return 0;
-    } 
+    }
 
     initscr();
     curs_set(0);
@@ -18,11 +20,26 @@ int main(int argc, char *argv[]) {
     WINDOW* win = newwin(20, 40, 5, 10);
     box(win, 0, 0);
     wrefresh(win);
-    
-    while ((ch = getch()) != 'q') {}
+
+    while ((ch = getch()) != 'q') {
+    }
 
     delwin(win);
     endwin();
+
+    /* тестовый вывод приложения для наглядности проделанной работы */
+    /* разрешено удаление данной части для дальнейшего решения */
+    Listdir* ldir = listdir_create(NULL);
+    if (fill_listdir(ldir)) {
+        printf("Error fill listdir!\n");
+        return 1;
+    }
+    for (; ldir != NULL; ldir = ldir->next) {
+        printf("path_dir: %s\n", ldir->path_dir);
+        for (; ldir->node != NULL; ldir->node = ldir->node->next) {
+            printf("name: %s | type: %d\n", ldir->node->name, ldir->node->type);
+        }
+    }
 
     return 0;
 }
