dd28c04faae88cfe126d594334e9dafa05fcdaf8
Садыганов Даниил Олегович <iv323s22@csc.sibsutis.ru>
2024-05-15 01:13:46 +0700

    Implemented funcs for reading and writing hidden objects, #5

--- src/opt.h
+++ src/opt.h
@@ -4,13 +4,14 @@
 #include <pwd.h>
 #include <unistd.h>
 
-#include "parser.h"
-#include "dir.h"
 #include "converter.h"
+#include "dir.h"
 #include "help.h"
+#include "parser.h"
 
 void opt_only_dir(char* path);
 void opt_m();
+void opt_a(char* path);
 void opt_a_no_dir();
 void selection_option(argv_t new_argv);
 
