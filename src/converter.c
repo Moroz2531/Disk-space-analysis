a104687f3c41b92ef518c075b101b7aa812fd7da
Садыганов Даниил Олегович <iv323s22@csc.sibsutis.ru>
2024-05-14 17:32:34 +0700

    Added units of measurement converter func, #10

--- src/converter.c
+++ src/converter.c
@@ -1,6 +1,6 @@
 #include "converter.h"
 
-size_t converter(size_t byte)
+size_t byte_converter(size_t byte)
 {
     // менее 1 кб - возвращаем число байт
     if (byte < KB)
@@ -16,4 +16,22 @@ size_t converter(size_t byte)
         return byte / GB;
 
     return -1;
+}
+
+char type_converter(size_t byte)
+{
+    // менее 1 кб - возвращаем тип байт
+    if (byte < KB)
+        return 'b';
+    // меньше 1 мб - возвращаем тип кб
+    if (byte < MB)
+        return 'K';
+    // меньше 1 гб - возвращаем тип мб
+    if (byte < GB)
+        return 'M';
+    // больше или равен 1 гб - возвращаем тип гб
+    if (byte >= GB)
+        return 'G';
+
+    return 'b';
 }
