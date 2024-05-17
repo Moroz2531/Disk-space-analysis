d9162e7977fc0723a33ba5c87cccae4e42b5a2f0
Denis-Nosov <dnosov537@gmail.com>
2024-05-17 16:56:42 +0700

    Create display listnode, #6

--- src/output.c
+++ src/output.c
@@ -1,3 +1,4 @@
+#include <dirent.h>
 #include <locale.h>
 #include <ncursesw/ncurses.h>
 #include <stdio.h>
@@ -11,7 +12,7 @@
 // отображение абсолютного корневого пути
 void display_root_path(Listdir* ldir)
 {
-    int length_path = strlen(ldir->path_dir);
+    const int length_path = strlen(ldir->path_dir);
     wchar_t buf[length_path + 1];
 
     swprintf(buf, length_path + 1, L"%s", ldir->path_dir);
@@ -19,19 +20,40 @@ void display_root_path(Listdir* ldir)
     mvaddwstr(0, strlen("path: "), buf);
 };
 
-// отображение имен файлов
+// отображение процентного соотношения
+int display_percentage(Listnode* n, const int y, const int x, size_t size_dir)
+{
+    // сдвиг по x
+    const int shift = x + 3;
+    const float percentage = ((float)n->byte / size_dir) * 100;
+    mvprintw(y, shift, "%5.2f%c\t", percentage, '%');
+    // обходим отображение процентного соотношения
+    return shift + 6;
+};
+
+void display_size(Listnode* n, const int y, const int x)
+{
+    const int shift = x + 4;
+    mvprintw(y, shift, "%ld b\t", n->byte);
+}
+
+// отображение содержимого каталога
 void display_listnode(Listdir* ldir)
 {
     Listnode* node = ldir->node;
-    for (int i = 2; node != NULL; node = node->next) {
+    for (int i = 2; node != NULL; node = node->next, i++) {
         wchar_t buf[SIZE_BUF];
         swprintf(buf, SIZE_BUF, L"%12s", node->name);
-        mvaddwstr(i++, 0, buf);
+        mvaddwstr(i, 0, buf);
+        const int x = display_percentage(node, i, SIZE_BUF, ldir->byte_dir);
+        display_size(node, i, x);
+        if (node->type == DT_DIR)
+            printw("#");
     }
 };
 
-// графика приложения: отображение разделителей и цветов
-void display_delim()
+// графика приложения: отображение разделителей, цветов и общей информации
+void display_delim(Listdir* ldir)
 {
     int cols, rows;
     getmaxyx(stdscr, rows, cols);
@@ -39,11 +61,6 @@ void display_delim()
     for (int i = 0; i < cols; i++)
         mvaddch(1, i, '-');
 };
-
-// отображение процентного соотношения
-void display_percentage(Listdir* ldir)
-{
-}
 
 // отображение структуры listdir
 int display_listdir(Listdir* ldir)
@@ -61,7 +78,7 @@ int display_listdir(Listdir* ldir)
 
         display_root_path(ldir);
         display_listnode(ldir);
-        display_delim();
+        display_delim(ldir);
 
         // перемещение курсора в начало
         move(2, 0);
